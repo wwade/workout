5d3fa3c317ea52c1bb712237c19f191cd0f7c6ee
wwade <wwade@users.noreply.github.com>
2026-06-22 19:43:12 -0700

    bump version to 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "dev.wwade.workout"
         minSdk = 26
         targetSdk = 37
-        versionCode = 4
-        versionName = "1.3.0"
+        versionCode = 5
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
