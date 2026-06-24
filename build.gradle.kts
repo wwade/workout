9dca0568fd4183cce8b935f2c590bf7660384067
wwade <wwade@users.noreply.github.com>
2026-06-24 13:00:14 -0700

    Add Gradle dependency locking and verification

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,9 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.ksp) apply false
 }
+
+allprojects {
+    dependencyLocking {
+        lockAllConfigurations()
+    }
+}
