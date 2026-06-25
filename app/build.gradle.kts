ed5ca9597e71766d54c329ce94a164fcda5a7d96
wwade <wwade@users.noreply.github.com>
2026-06-24 18:58:39 -0700

    Version 1.4.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "dev.wwade.workout"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.4.0"
+        versionCode = 6
+        versionName = "1.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
